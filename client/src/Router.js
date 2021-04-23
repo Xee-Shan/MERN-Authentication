3c664faf3579fba62d0eebf075790da99771b3a3
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2021-04-23 14:48:22 +0500

    fsdg

--- client/src/Router.js
+++ client/src/Router.js
@@ -23,6 +23,7 @@ export default function Router() {
         { loggedIn === true &&
         <Route path="/customer" component={Customer}/>
         }
+      {/* sdafkjlskdajflkjafkjdslakjflkasjdf */}
       </Switch>
         </div>
     )
